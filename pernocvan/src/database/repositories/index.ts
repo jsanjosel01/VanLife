ea1bd52795caa8a09e8e14b995f2ac8c9fc8a896
Yuls <152199674+jsanjosel01@users.noreply.github.com>
2026-05-22 13:00:22 +0200

    Cambiar nombre
    cambiar nombre a la carpeta

--- pernocvan/src/database/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-// src/database/repositories/index.ts
-import { SupabaseUserRepository } from '../supabase/SupabaseUserRepository';
-import type { UserRepository } from './UserRepository';
-
-
-// Aquí decides que la implementación real es Supabase
-export const userRepository: UserRepository = SupabaseUserRepository;
